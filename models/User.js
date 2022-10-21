0f799e4a1411d8734b91a2cdfac9226c38c91ece
unknown <gaskoviktor1@gmail.com>
2022-10-21 13:05:42 +0200

    last conflict resolved

--- models/User.js
+++ models/User.js
@@ -1,13 +1,9 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
 const userSchema = new Schema({
-<<<<<<< HEAD
   googleId: String,
   credits: { type: Number, default: 0 },
-=======
-  googleId: String
->>>>>>> b7bb14e24a61cc18ea694c85cc628c3527e9a3a4
 });
 
-mongoose.model('users', userSchema);
+mongoose.model("users", userSchema);
