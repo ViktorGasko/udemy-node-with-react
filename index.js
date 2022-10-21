4c952f0f23e22d7493e62fb8903c184a5e9e442c
unknown <gaskoviktor1@gmail.com>
2022-10-21 12:52:08 +0200

    Section 9

--- index.js
+++ index.js
@@ -1,30 +1,37 @@
-//node v dobe tvorby kurzu nepodporoval import express from ...
 const express = require("express");
 const mongoose = require("mongoose");
+const cookieSession = require("cookie-session");
+const passport = require("passport");
+const bodyParser = require("body-parser");
+const keys = require("./config/keys");
 require("./models/User");
 require("./services/passport");
-const keys = require("./config/keys");
-const passport = require("passport");
-const cookieSession = require("cookie-session");
 
-mongoose.connect(keys.mongoUri);
+mongoose.connect(keys.mongoURI);
+
 const app = express();
 
+app.use(bodyParser.json());
 app.use(
   cookieSession({
-    maxAge: 30 * 24 * 3600000, //30 dni v ms
+    maxAge: 30 * 24 * 60 * 60 * 1000,
     keys: [keys.cookieKey],
   })
 );
-
 app.use(passport.initialize());
 app.use(passport.session());
 
-require("./routes/authRoutes")(app);
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
 
-// app.get("/", (req, res) => {
-//   res.send({ hi: "there" });
-// });
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
+require("./routes/authRoutes")(app);
+require("./routes/billingRoutes")(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
