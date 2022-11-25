e8832e90946e55540fb71e8fe26316e8937d88fc
unknown <gaskoviktor1@gmail.com>
2022-11-25 09:08:12 +0100

    survey models added and route added

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cookieSession = require("cookie-session");
 const passport = require("passport");
 const bodyParser = require("body-parser");
 const keys = require("./config/keys");
+require("./models/Survey");
 require("./models/User");
 require("./services/passport");
 
@@ -32,6 +33,7 @@ if (process.env.NODE_ENV === "production") {
 
 require("./routes/authRoutes")(app);
 require("./routes/billingRoutes")(app);
+require("./routes/surveyRoutes")(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
